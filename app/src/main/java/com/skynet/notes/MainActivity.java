d126e5debc2088adf92cba0c194c070f671b531e
Sky Net <davidbishop8000@gmail.com>
2022-04-22 12:45:36 -0900

    added editNoteActivity

--- app/src/main/java/com/skynet/notes/MainActivity.java
+++ app/src/main/java/com/skynet/notes/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -20,18 +22,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button addButton = findViewById(R.id.addButton);
+        final Button addButton = findViewById(R.id.addButton);
+        final RecyclerView recyclerView = findViewById(R.id.NotesRecycleView);
         String date = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
         notes.add(new Note ("My first note", date, null));
         notes.add(new Note ("My second note", date, null));
         notes.add(new Note ("My third note", date, null));
-        RecyclerView recyclerView = findViewById(R.id.NotesRecycleView);
+
         NoteAdapter.NoteClickListener noteClickListener = (note, position) -> Toast.makeText(getApplicationContext(),
                 "you touch " + note.getTitle(), Toast.LENGTH_SHORT).show();
 
         NoteAdapter adapter = new NoteAdapter(this, notes, noteClickListener);
         recyclerView.setAdapter(adapter);
-        addButton.setOnClickListener(v -> Toast.makeText(getApplicationContext(),
-                "new note add ", Toast.LENGTH_SHORT).show());
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "new note add ", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, NoteEditActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/skynet/notes/NoteEditActivity.java
@@ -0,0 +1,42 @@
+package com.skynet.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NoteEditActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_edit);
+        final Button backButton = findViewById(R.id.backButton);
+        final Button saveButton = findViewById(R.id.saveButton);
+        backButton.setOnClickListener(this);
+        saveButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        Intent intent = new Intent(NoteEditActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        switch (v.getId()) {
+            case R.id.backButton:
+                startActivity(intent);
+                break;
+            case R.id.saveButton:
+                if (TextUtils.isEmpty(((EditText)findViewById(R.id.editTitle)).getText().toString())){
+                    Toast.makeText(getApplicationContext(),"note title is empty ", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "note saved ", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+}
